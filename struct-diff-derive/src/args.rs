7b511f59392e3bb6cf01947ab064bf3df357a63b
Philip Degarmo <aclysma@gmail.com>
2019-10-06 03:53:13 -0700

    WIP proc macro works for copy types

--- struct-diff-derive/src/args.rs
+++ struct-diff-derive/src/args.rs
@@ -10,88 +10,49 @@ pub struct StructDiffStructArgs {
     pub ident: syn::Ident,
 }
 
-// We support multiple distinct inspect annotations (i.e. diff_copy, diff_clone, diff_custom)
-// Each distinct type will have a struct for capturing the metadata. These metadata structs
-// must implement this trait
-pub trait StructDiffFieldArgs {
-    fn ident(&self) -> &Option<syn::Ident>;
-    fn ty(&self) -> &syn::Type;
-    fn skip(&self) -> bool;
-}
 
 //
-// Copy field handling
+// Metadata from the struct's field annotations
 //
 #[derive(Debug, FromField, Clone)]
 #[darling(attributes(diffable))]
-pub struct StructDiffFieldArgsCopy {
+pub struct StructDiffFieldArgs {
     ident: Option<syn::Ident>,
     ty: syn::Type,
 
     #[darling(default)]
-    skip: bool
-}
-
-impl StructDiffFieldArgs for StructDiffFieldArgsCopy {
-    fn ident(&self) -> &Option<syn::Ident> {
-        &self.ident
-    }
-    fn ty(&self) -> &syn::Type {
-        &self.ty
-    }
-    fn skip(&self) -> bool {
-        self.skip
-    }
-}
-
-
-//
-// Clone field handling
-//
-#[derive(Debug, FromField, Clone)]
-#[darling(attributes(diffable))]
-pub struct StructDiffFieldArgsClone {
-    ident: Option<syn::Ident>,
-    ty: syn::Type,
+    skip: bool,
 
     #[darling(default)]
-    skip: bool
-}
-
-impl StructDiffFieldArgs for StructDiffFieldArgsClone {
-    fn ident(&self) -> &Option<syn::Ident> {
-        &self.ident
-    }
-    fn ty(&self) -> &syn::Type {
-        &self.ty
-    }
-    fn skip(&self) -> bool {
-        self.skip
-    }
-}
-
-
-//
-// Custom field handling
-//
-#[derive(Debug, FromField, Clone)]
-#[darling(attributes(diffable))]
-pub struct StructDiffFieldArgsCustom {
-    ident: Option<syn::Ident>,
-    ty: syn::Type,
+    clone: bool,
 
     #[darling(default)]
-    skip: bool
+    copy: bool,
+
+    #[darling(default)]
+    custom: bool,
+
+    #[darling(default)]
+    diff_type: Option<syn::Path>
 }
 
-impl StructDiffFieldArgs for StructDiffFieldArgsCustom {
-    fn ident(&self) -> &Option<syn::Ident> {
-        &self.ident
+impl StructDiffFieldArgs {
+    pub fn ident(&self) -> &Option<syn::Ident> {
+        return &self.ident
     }
-    fn ty(&self) -> &syn::Type {
-        &self.ty
+    pub fn ty(&self) -> &syn::Type {
+        return &self.ty
     }
-    fn skip(&self) -> bool {
-        self.skip
+    pub fn skip(&self) -> bool {
+        return self.skip
+    }
+    pub fn copy(&self) -> bool {
+        return self.copy
+    }
+    pub fn custom(&self) -> bool {
+        return self.custom
+    }
+    pub fn diff_type(&self) -> &Option<syn::Path> {
+        return &self.diff_type
     }
 }
--- struct-diff-derive/src/lib.rs
+++ struct-diff-derive/src/lib.rs
@@ -13,94 +13,65 @@ pub fn inspect_macro_derive(input: proc_macro::TokenStream) -> proc_macro::Token
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     let struct_args = args::StructDiffStructArgs::from_derive_input(&input).unwrap();
     let parsed_fields = parse_fields(&input);
-    generate(&input, struct_args, parsed_fields)
+
+    // Check all parsed fields for any errors that may have occurred
+    let mut ok_fields : Vec<ParsedField> = vec![];
+    let mut errors = vec![];
+    for pf in parsed_fields {
+        match pf {
+            Ok(value) => ok_fields.push(value),
+            Err(e) => errors.push(e)
+        }
+    }
+
+    // If any error occurred, return them all here
+    if !errors.is_empty() {
+        return proc_macro::TokenStream::from(darling::Error::multiple(errors).write_errors());
+    }
+
+    // Go ahead and generate the code
+    generate(&input, struct_args, ok_fields)
 }
 
-fn handle_inspect_type<
-    FieldArgsT: darling::FromField + args::StructDiffFieldArgs + Clone,
-    //ArgsT: From<FieldArgsT> + ToTokens,
->(
-    parsed_field: &mut Option<ParsedField>,
+fn get_diff_type(field_args: &args::StructDiffFieldArgs) -> syn::Path {
+    // This type is the "diff" representation for the given field
+    // - Copy/Clone: Will be "T" - i.e. a diff of an f32 is stored as an f32
+    // - Custom: By default, tacks "Diff" to the end of the type. This mirrors the behavior
+    //   for deriving "Diffable" on a struct. But this can be overridden by setting diff_type
+    //   in the field attribute
+    field_args.diff_type().clone().unwrap_or_else(|| {
+        let ty = field_args.ty();
+        let diff_type = if field_args.custom() {
+            // Handles the case of a custom diff where diff_type is unspecified. Default it to
+            // the struct name we would produce for the given type
+            let s = format!("{}Diff", quote!(#ty));
+            let ty = quote::format_ident!("{}", s);
+            quote!(#ty)
+        } else {
+            // Handles the copy/clone case. Just return the type
+            quote!(#ty)
+        };
+
+        syn::parse2::<syn::Path>(quote!{#diff_type}).unwrap()
+    })
+}
+
+fn parse_field(
     f: &syn::Field,
-    //default_render_trait: proc_macro2::TokenStream,
-    //arg_type: proc_macro2::TokenStream,
-)
+) -> Result<ParsedField, darling::Error>
 {
-    //TODO: Improve error message
-    if parsed_field.is_some() {
-        panic!(
-            "Too many inspect attributes on a single member {:?}",
-            f.ident
-        );
-    }
+    //TODO: Unwrapping is less clear, figure out how to return
+    use darling::FromField;
+    let field_args = args::StructDiffFieldArgs::from_field(&f)?;
+    let diff_type = get_diff_type(&field_args);
 
-    let field_args = FieldArgsT::from_field(&f).unwrap();
-
-    if field_args.skip() {
-        *parsed_field = Some(ParsedField {
-            //skip: true
-        });
-
-        return;
-    }
-
-
-    *parsed_field = Some(ParsedField {
-        //skip: true
-    });
+    Ok(ParsedField {
+        field_args,
+        diff_type
+    })
 }
 
-fn try_handle_inspect_type<
-    FieldArgsT: darling::FromField + args::StructDiffFieldArgs + Clone,
-    //ArgsT: From<FieldArgsT> + ToTokens,
->(
-    parsed_field: &mut Option<ParsedField>,
-    f: &syn::Field,
-    path: &syn::Path,
-    //default_render_trait: proc_macro2::TokenStream,
-    //arg_type: proc_macro2::TokenStream,
-) {
-    if f.attrs.iter().find(|x| x.path == *path).is_some() {
-        handle_inspect_type::<FieldArgsT/*, ArgsT*/>(parsed_field, &f/*, default_render_trait, arg_type*/);
-    }
-}
-
-
-fn handle_inspect_types(parsed_field: &mut Option<ParsedField>, f: &syn::Field) {
-    // These are effectively constants
-    #[allow(non_snake_case)]
-    let STRUCT_DIFF_COPY_PATH = syn::parse2::<syn::Path>(quote!(diff_copy)).unwrap();
-    #[allow(non_snake_case)]
-    let STRUCT_DIFF_CLONE_PATH = syn::parse2::<syn::Path>(quote!(diff_clone)).unwrap();
-    #[allow(non_snake_case)]
-    let STRUCT_DIFF_CUSTOM_PATH = syn::parse2::<syn::Path>(quote!(diff_custom)).unwrap();
-
-    // We must check every trait
-    try_handle_inspect_type::<args::StructDiffFieldArgsCopy>(
-        parsed_field,
-        f,
-        &STRUCT_DIFF_COPY_PATH,
-        //quote!(imgui_inspect::InspectRenderSlider),
-        //quote!(imgui_inspect::InspectArgsSlider),
-    );
-
-    try_handle_inspect_type::<args::StructDiffFieldArgsClone>(
-        parsed_field,
-        f,
-        &STRUCT_DIFF_CLONE_PATH,
-        //quote!(imgui_inspect::InspectRenderDefault),
-        //quote!(imgui_inspect::InspectArgsDefault),
-    );
-    try_handle_inspect_type::<args::StructDiffFieldArgsCustom>(
-        parsed_field,
-        f,
-        &STRUCT_DIFF_CUSTOM_PATH,
-        //quote!(imgui_inspect::InspectRenderDefault),
-        //quote!(imgui_inspect::InspectArgsDefault),
-    );
-}
-
-fn parse_fields(input: &syn::DeriveInput) -> Vec<ParsedField> {
+fn parse_fields(input: &syn::DeriveInput) -> Vec<Result<ParsedField, darling::Error>> {
 
     use syn::Data;
     use syn::Fields;
@@ -109,33 +80,11 @@ fn parse_fields(input: &syn::DeriveInput) -> Vec<ParsedField> {
         Data::Struct(ref data) => {
             match data.fields {
                 Fields::Named(ref fields) => {
-                    // Parse the fields
-                    let parsed_fields: Vec<_> = fields
+                    fields
                         .named
                         .iter()
-                        .map(|f| {
-                            let mut parsed_field: Option<ParsedField> = None;
-
-                            handle_inspect_types(&mut parsed_field, &f);
-
-                            if parsed_field.is_none() {
-                                handle_inspect_type::<args::StructDiffFieldArgsCopy>(
-                                    &mut parsed_field,
-                                    &f,
-                                    //quote!(imgui_inspect::InspectRenderDefault),
-                                    //quote!(imgui_inspect::InspectArgsDefault),
-                                );
-                                //parsed_field = Some(ParsedField {});
-                            }
-
-                            // We expect the previous code to either successfully parse an attribute
-                            // and set parsed_field to a non-none value or panic with a descriptive
-                            // error message
-                            parsed_field.unwrap()
-                        })
-                        .collect();
-
-                    parsed_fields
+                        .map(|f| parse_field(&f))
+                        .collect()
                 }
                 //Fields::Unit => ,
                 _ => unimplemented!(),
@@ -145,15 +94,116 @@ fn parse_fields(input: &syn::DeriveInput) -> Vec<ParsedField> {
     }
 }
 
+#[derive(Debug)]
 struct ParsedField {
     //render: proc_macro2::TokenStream,
     //render_mut: proc_macro2::TokenStream,
+    field_args: args::StructDiffFieldArgs,
+    diff_type: syn::Path
 }
 
 fn generate(
-    _input: &syn::DeriveInput,
-    _struct_args: args::StructDiffStructArgs,
-    _parsed_fields: Vec<ParsedField>,
-) -> proc_macro::TokenStream {
-    return proc_macro::TokenStream::from(quote! {})
+    input: &syn::DeriveInput,
+    struct_args: args::StructDiffStructArgs,
+    parsed_fields: Vec<ParsedField>,
+) -> proc_macro::TokenStream
+{
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let mut diff_struct_fields = vec![];
+    for pf in &parsed_fields {
+
+        let diff_ident = pf.field_args.ident().clone();
+        let diff_type = pf.diff_type.clone();
+
+        diff_struct_fields.push(quote!{
+            #diff_ident: Option<#diff_type>
+        })
+    }
+
+    let struct_name = &struct_args.ident;
+    let diff_struct_name = quote::format_ident!("{}Diff", struct_name);
+
+    let diff_struct = quote! {
+        #[derive(Default)]
+        struct #diff_struct_name {
+            #(#diff_struct_fields),*
+        }
+    };
+
+    let mut diff_fn_field_handlers = vec![];
+    for pf in &parsed_fields {
+
+        let ident = pf.field_args.ident().clone();
+        let ty = pf.field_args.ty().clone();
+        let diffable_trait = quote::format_ident!("DiffableByCopy");
+
+        diff_fn_field_handlers.push(quote!{
+            {
+                {
+                    let member_diff = <#ty as #diffable_trait<_, _>>::diff(&old.#ident, &new.#ident);
+                    if member_diff.is_some() {
+                        struct_diff.#ident = member_diff;
+                        has_change = true;
+                    }
+                }
+            }
+        });
+    }
+
+    let diff_fn = quote! {
+        fn diff(old: &#struct_name, new: &#struct_name) -> Option<#diff_struct_name> {
+            let mut struct_diff = #diff_struct_name::default();
+            let mut has_change = false;
+
+            #(#diff_fn_field_handlers)*
+
+//          {
+//              {
+//                  let member_diff = <Vec<String> as DiffableByCustom<_, _>>::diff(&old.string_list, &new.string_list);
+//                  if member_diff.is_some() {
+//                      struct_diff.string_list = member_diff;
+//                      has_change = true;
+//                  }
+//              }
+//          }
+
+            if has_change {
+                Some(struct_diff)
+            } else {
+                None
+            }
+        }
+    };
+
+    let mut apply_fn_field_handlers = vec![];
+    for pf in &parsed_fields {
+        let ident = pf.field_args.ident().clone();
+        apply_fn_field_handlers.push(quote!{
+            if let Some(diff) = diff {
+                if let Some(#ident) = diff.#ident {
+                    target.#ident = #ident;
+                }
+            }
+        });
+    }
+
+    let apply_fn = quote! {
+        fn apply(diff: &Option<#diff_struct_name>, target: &mut #struct_name) {
+            #(#apply_fn_field_handlers)*
+        }
+    };
+
+
+    let diff_impl = quote! {
+        impl DiffableByCustom<#struct_name, Option<#diff_struct_name>> for #struct_name {
+            #diff_fn
+            #apply_fn
+        }
+    };
+
+    return proc_macro::TokenStream::from(quote! {
+        #diff_struct
+        #diff_impl
+    })
 }

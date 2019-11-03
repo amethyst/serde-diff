e50b1e491bbd80117f0cd21ef48c690106c1541d
Philip Degarmo <aclysma@gmail.com>
2019-11-03 11:36:38 -0800

    Rename serde_diffable module to serde_diff

--- struct-diff-derive/src/lib.rs
+++ struct-diff-derive/src/lib.rs
@@ -1,8 +1,8 @@
 extern crate proc_macro;
 
-mod serde_diffable;
+mod serde_diff;
 
 #[proc_macro_derive(SerdeDiff, attributes(serde_diff))]
 pub fn serde_diff_macro_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    serde_diffable::macro_derive(input)
+    serde_diff::macro_derive(input)
 }
